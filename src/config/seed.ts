53ffdb2cb6395ebca03fbfbf6dc9fd85e4e6b4d5
Syneras <imasyneras@gmail.com>
2025-11-08 09:09:28 +0700

    Upgrade Deploy method

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -19,17 +19,17 @@ async function main() {
 
     await prisma.user.upsert({
         where: { email: 'master@master.com' },
-        update: { password: await PasswordUtil.hashPassword('Syne.263') }, 
+        update: { password: await PasswordUtil.hashPassword('Abc@12345') }, 
         create: {
             firstName: 'Master',
             lastName: 'Admin',
             username: 'Admin',
-            phone: '0563343245',
+            phone: '0123456789',
             age: 20,
             emailVerified: true,
             gender: Gender.male,
-            email: 'imasyneras@gmail.com',
-            password: await PasswordUtil.hashPassword('Syne.263'),
+            email: 'master@master.com',
+            password: await PasswordUtil.hashPassword('Abc@12345'),
 
             userRoles: {
                 create: [
